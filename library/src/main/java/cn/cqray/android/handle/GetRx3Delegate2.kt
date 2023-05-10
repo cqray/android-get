c41b7721339aa236a779f6648c67cab09bc770cf
PC <soft.leijue@qq.com>
2023-05-10 18:20:36 +0800

    initial commit

---
+++ library/src/main/java/cn/cqray/android/handle/GetRx3Delegate2.kt
@@ -0,0 +1,32 @@
+package cn.cqray.android.handle
+
+class GetRx3Delegate2 : GetRxDelegate2 {
+
+    override fun addDisposable(tag: Any?, disposable: Any) {
+        TODO("Not yet implemented")
+    }
+
+    override fun addDisposables(tag: Any?, vararg disposables: Any) {
+        TODO("Not yet implemented")
+    }
+
+    override fun clearDisposables(tag: Any?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun clearAllDisposables() {
+        TODO("Not yet implemented")
+    }
+
+    override fun dispose(tag: Any?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun disposeAll() {
+        TODO("Not yet implemented")
+    }
+
+//    override fun clearDisposables() {
+//        TODO("Not yet implemented")
+//    }
+}
